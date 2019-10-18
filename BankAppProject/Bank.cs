d651ae2d1ccd19abb733633ac576581d8aabbb6e
Huy Tran <Nicudes@gmail.com>
2019-10-18 09:23:58 +0200

    Updates!

--- BankAppProject/Bank.cs
+++ BankAppProject/Bank.cs
@@ -30,7 +30,7 @@ namespace BankAppProject
             //Create a date of account creation, can be hardcoded
             //Create account automatic from the method CreateAccount
             //Writes client name and the ID that was created by the Client class
-
+            Console.WriteLine();
             ShowClient();
         }
     
