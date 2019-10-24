2de4d14ea85fcfc46a4dfb20ef3f1e182525ec51
Mikael <m.klaening@gmail.com>
2019-10-24 10:24:58 +0200

    Added class Colours with methods for recolouring

---
+++ BankAppProject/Colours.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAppProject
+{
+    class Colours
+    {
+        public static void Red(string input)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write(input);
+            Console.ResetColor();
+        }
+
+        public static void Green(string input)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write(input);
+            Console.ResetColor();
+        }
+
+        public static void Cyan(string input)
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Write(input);
+            Console.ResetColor();
+        }
+    }
+}
